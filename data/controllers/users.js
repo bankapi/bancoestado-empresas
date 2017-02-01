deea27bd11b6cb444bbf9aa5b253c546ca12988c
jaime-e <jechegarayz@gmail.com>
2017-02-01 17:37:45 -0300

    [task] require account credentials when creating a new user

--- data/controllers/users.js
+++ data/controllers/users.js
@@ -1,19 +1,32 @@
 'use strict'
 
-const usersModel = require('../models/').Users
+const models = require('../models/')
 const apiKeysController = require('./apikeys')
+const isChileRut = require('../validators').isChileRut
 
 function create (opts) {
   return new Promise((resolve, reject) => {
-    usersModel.create(opts)
+    if (!(opts.email && opts.credentials && isChileRut(opts.credentials.rutCompany) && isChileRut(opts.credentials.rutUser) && opts.credentials.password)) {
+      return reject(new Error('required email and credentials:rutCompany,rutUser,password'))
+    }
+
+    models.Users.create(opts)
     .then((user) => {
-      apiKeysController.create({UserId: user.id})
+      opts.UserId = user.id
+      apiKeysController.create(opts)
       .then((key) => {
-        let result = {
-          user: user.get({plain: true}),
-          apiKey: key
-        }
-        resolve(result)
+        models.Accounts.create(opts)
+        .then((account) => {
+          let result = {
+            user: user.get({plain: true}),
+            apiKey: key,
+            account: account.get({plain: true})
+          }
+          resolve(result)
+        })
+        .catch((err) => {
+          reject(err)
+        })
       })
       .catch((err) => {
         reject(err)
