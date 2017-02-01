1e804ffd2f95d56b00db86ff4607d75c41bd1f70
jaime-e <jechegarayz@gmail.com>
2017-02-01 12:17:11 -0300

    [fix] transaction model definition and when saving transaction to DB use findOrCreate instead of create

--- data/models/transactions.js
+++ data/models/transactions.js
@@ -2,8 +2,10 @@
 
 module.exports = function (sequelize, DataTypes) {
   var Transactions = sequelize.define('Transactions', {
-    data: DataTypes.JSONB,
-    unique: true
+    data: {
+      type: DataTypes.JSONB,
+      unique: true
+    }
   }, {
     classMethods: {
       associate: function (models) {
